b3e89dcadce00495e278f99abe9d827709331988
Viktor Andersson <viktor.andersson@mindified.com>
2025-09-23 10:27:13 +0200

    feat: turn into pwa app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bb-clock/',
   plugins: [
-    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Blood Bowl Clock',
+        short_name: 'bb-clock',
+        description: 'Game clock for Blood Bowl or similar games where you have a turn timer and a game time pool as backup.',
+        theme_color: '#457200ff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     tailwindcss()
   ],
 })
