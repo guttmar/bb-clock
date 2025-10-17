103bce94fc7d1f1c42fe953fc5c75d099998d0c5
Viktor Andersson <viktor.andersson@mindified.com>
2025-10-17 20:36:44 +0200

    feat: persist settings in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ClockPage from "./pages/ClockPage";
 import SettingsPage from "./pages/SettingsPage";
+import { usePersistedState } from "./hooks/usePersistedState";
 
 export default function App() {
-  const [p1TurnTime, setP1TurnTime] = useState(2 * 60 * 1000); // 2 minutes
-  const [p1PoolTime, setP1PoolTime] = useState(5 * 60 * 1000); // 5 minutes
-  const [p2TurnTime, setP2TurnTime] = useState(2 * 60 * 1000); // 2 minutes
-  const [p2PoolTime, setP2PoolTime] = useState(5 * 60 * 1000); // 5 minutes
+  const [p1TurnTime, setP1TurnTime] = usePersistedState("p1TurnTime", 2 * 60 * 1000); // 2 minutes
+  const [p1PoolTime, setP1PoolTime] = usePersistedState("p1PoolTime", 5 * 60 * 1000); // 5 minutes
+  const [p2TurnTime, setP2TurnTime] = usePersistedState("p2TurnTime", 2 * 60 * 1000); // 2 minutes
+  const [p2PoolTime, setP2PoolTime] = usePersistedState("p2PoolTime", 5 * 60 * 1000); // 5 minutes
 
   return (
     <Router>
